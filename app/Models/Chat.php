ad477dd9d891fa0f1adda07eadd0776981521be1
kayes <kayes3441@gmail.com>
2025-08-29 16:04:22 +0600

    issue fixing

---
+++ app/Models/Chat.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Chat extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'message',
+        'attachments',
+        'read_at',
+    ];
+
+    protected $casts = [
+        'attachments' => 'array',
+        'read_at' => 'datetime',
+    ];
+
+    public function sender():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver():BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
